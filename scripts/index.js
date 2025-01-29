95d516d76bf390bbd18f5a4593909c29ee48ef53
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2025-01-29 23:32:44 +0100

    Updates to icons

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,34 @@
 function onLoad(){
-    document.documentElement.style.width = `${getWindowWidth()}px`;
-    document.documentElement.style.height = `${getWindowHeight()}px`;
-    document.getElementById("header-text").innerHTML = `Phillip Johnerud - ${getWindowWidth()}px`;
-    if ( getWindowWidth() < 1370) {
-        document.getElementById("main-top-text").style.width = "40rem";
-        document.getElementById("code-example").style.width = "40rem";
-        document.getElementById("sleepy-time").style.width = "30rem";
-        document.getElementById("knowledge").style.width = "40rem";
-        document.getElementById("main-bottom-text-r").style.marginTop = "0rem";
-    }
-    if ( getWindowWidth() > 1370) {
-        document.getElementById("main-top-text").style.width = "20rem";
-        document.getElementById("code-example").style.width = "20rem";
-        document.getElementById("sleepy-time").style.width = "30rem";
-        document.getElementById("knowledge").style.width = null;
-        document.getElementById("main-bottom-text-r").style.marginTop = "30rem";
-    }
+    stuff()
     console.log( getWindowWidth())
 }
 
 function onResize(){
+
+    stuff()
+    console.log( getWindowWidth())
+}
+
+function stuff(){
+
     document.documentElement.style.width = `${getWindowWidth()}px`;
     document.documentElement.style.height = `${getWindowHeight()}px`;
     document.getElementById("header-text").innerHTML = `Phillip Johnerud - ${getWindowWidth()}px`;
+
     if ( getWindowWidth() < 1370) {
         document.getElementById("main-top-text").style.width = "40rem";
         document.getElementById("knowledge").style.width = "40rem";
         document.getElementById("sleepy-time").style.width = "30rem";
         document.getElementById("main-bottom-text-r").style.marginTop = "0rem";
+
+        document.getElementById("github").style.width = "50";
+        document.getElementById("github").style.height = "50"; //height="32" width="32"
+
+        document.getElementById("linkedin").style.width = "60";
+        document.getElementById("linkedin").style.height = "60"; //height="40" width="40"
+
+        document.getElementById("mail").style.width = "78";
+        document.getElementById("mail").style.height = "78"; //height="48" width="48"
     }
     if ( getWindowWidth() > 1370) {
         document.getElementById("main-top-text").style.width = "20rem";
@@ -35,9 +36,20 @@ function onResize(){
         document.getElementById("sleepy-time").style.width = "20rem";
         document.getElementById("knowledge").style.width = null;
         document.getElementById("main-bottom-text-r").style.marginTop = "30rem";
+
+
+        document.getElementById("github").style.width = "32";
+        document.getElementById("github").style.height = "32"; //height="32" width="32"
+
+        document.getElementById("linkedin").style.width = "40";
+        document.getElementById("linkedin").style.height = "40"; //height="40" width="40"
+
+        document.getElementById("mail").style.width = "48";
+        document.getElementById("mail").style.height = "48"; //height="48" width="48"
     }
-    console.log( getWindowWidth())
 }
+
+
 function somethingChange(){
     let x = getById("blue-code");
     let y = getById("green-code");
