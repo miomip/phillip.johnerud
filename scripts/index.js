782cdf8c8b66358ca20c99ed01d130de4854f245
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2025-03-15 19:01:25 +0100

    Optimizations and Easter eggs

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,21 @@ const aboutAnchor = document.body.querySelector('#about-anchor');
 const aboutMenu = document.body.querySelector('#about-menu');
 
 aboutAnchor.addEventListener('click', () => { aboutMenu.open = !aboutMenu.open; });
+
+const changeColor = document.body.querySelector('#change-color');
+let state = false;
+
+changeColor.addEventListener('click', () => {
+    state = !state;
+    if (state) {
+        document.documentElement.style.setProperty('--green-code', 'white');
+        document.documentElement.style.setProperty('--blue-code', 'white');
+        document.documentElement.style.setProperty('--orange-code', 'white');
+        document.documentElement.style.setProperty('--magenta-code', 'white');
+    }else {
+        document.documentElement.style.setProperty('--green-code', '#6aab73');
+        document.documentElement.style.setProperty('--blue-code', '#56a8f5');
+        document.documentElement.style.setProperty('--orange-code', '#cf8e6d');
+        document.documentElement.style.setProperty('--magenta-code', '#c47cba');
+    }
+});
